2e733d351c328d8a729a95b0a6b89a941cd31218
gfredex <g.fredex@gmail.com>
2024-10-07 15:46:05 +0300

    add: added solution to task 2 light level

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Задача 1 Легкий уровень
 const task11 = document.querySelector('.wrap-task11');
-const btn11 = document.getElementById('btn11');
+
 task11.addEventListener('click', (e) => {
     if (e.target.id === 'btn11') {
         document.querySelector('.p11').textContent = 'Была нажата "Кнопка 1"';
@@ -13,4 +13,17 @@ task11.addEventListener('click', (e) => {
     }
     // console.log(e.target.id === 'btn11');
 
+});
+
+//Задача 2 Легкий уровень
+const form12 = document.getElementById('form12');
+form12.addEventListener('click', (e) => {
+    let data = '';
+    if (e.target.id === 'btn21') {
+        if (form12.children.input21.value !== '') {
+            document.querySelector('#list12').append(document.createElement("li"));
+            document.querySelector('#list12').lastChild.textContent = form12.children.input21.value.trim();
+            form12.children.input21.value = '';
+        }
+    }
 });
